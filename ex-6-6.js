7295459178262591d76f412df252f61e852cd805
zm16591 <zm16591@josai.ac.jp>
2018-10-21 21:57:55 -0400

    message

---
+++ ex-6-6.js
@@ -0,0 +1,15 @@
+var who66 = 'world';
+// var come = 'が来た'
+function sayhello66() {
+// var who = 'world';
+  alert('Hello, ' + who66 + '!');
+} 
+
+function taro() {
+  who66 = '太郎' ;
+} 
+function hanako() {
+  who66 = '花子';
+}
+function gakita() {alert(who66 + 'が来た');
+}
---
+++ ex-6-8.js
@@ -0,0 +1,9 @@
+function sayhello68(){
+alert('Hello,'+document.getElementByld('who68').innerHTML+'!');
+}
+function taro68(){
+document.getElementByld('who68').innerHTML='太郎';
+}
+function hanako68(){
+document.getElementByld('who68').innerHTML='花子';
+}
